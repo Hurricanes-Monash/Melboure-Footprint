3996810edf2856d295afc52baf665979b166993d
Zihao Wang <zihaowang@zihaos-macbook-pro.local>
2016-08-18 02:45:55 +1000

    1.0

--- melbourne1/ViewController.swift
+++ melbourne1/ViewController.swift
@@ -7,11 +7,45 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+  
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var imageView: UIImageView!
+    let imageUrlload:String? = "https://rexwh.files.wordpress.com/2013/11/colesfountain_20131117_1342_46.jpg"
     override func viewDidLoad() {
         super.viewDidLoad()
+         let ref = Firebase(url: "https://melbournefootprint.firebaseio.com/artworks")
+        ref.childByAppendingPath("name").observeSingleEventOfType(.Value, withBlock: { snapshot in
+            print( snapshot.value )
+            self.nameLabel.text = String(snapshot.value)
+            })
+        ref.childByAppendingPath("description").observeSingleEventOfType(.Value, withBlock: { snapshot in
+            print( snapshot.value )
+            self.textView.text = String(snapshot.value)
+        })
+                imageView.image = nil
+        if let imageUrl = imageUrlload {
+            NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: imageUrl)!,completionHandler: {(data,response,error) -> Void in
+                if error != nil {
+                print(error)
+                    return
+                }
+            let image = UIImage(data: data!)
+                dispatch_async(dispatch_get_main_queue(), {() -> Void in
+                    self.imageView.image = image
+                    ;
+                    
+            
+                })
+            
+            }).resume()
+            
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +54,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
 
 }
 
