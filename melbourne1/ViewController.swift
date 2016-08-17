539c7f10346979e73574f2b656a670f309b44105
Zihao Wang <zihaowang@zihaos-macbook-pro.local>
2016-08-17 22:49:34 +1000

    Initial Commit

---
+++ melbourne1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  melbourne1
+//
+//  Created by zihaowang on 17/08/2016.
+//  Copyright © 2016 zihaowang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
