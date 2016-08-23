1c05352fcb73d9feb1516eedafd000c3e886e342
Michael0770 <hyman0770@gmail.com>
2016-08-23 14:54:43 +1000

    Initial Commit

---
+++ melbourne1/VCMapView.swift
@@ -0,0 +1,134 @@
+//
+//  VCMapView.swift
+//  HonoluluArt
+//
+//  Created by Audrey M Tam on 17/10/2014.
+//  Copyright (c) 2014 Ray Wenderlich. All rights reserved.
+//
+
+import MapKit
+
+extension MapViewController: MKMapViewDelegate {
+    
+    
+    
+    func mapView(mapView: MKMapView,
+                 viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView! {
+        if let annotation = annotation as? ArtworkForMap {
+            let identifier = "pin"
+            var view: MKAnnotationView
+            if let dequeuedView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier)
+                as? MKPinAnnotationView { // 2
+                dequeuedView.annotation = annotation
+                view = dequeuedView
+            } else {
+                // 3
+                view = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                view.canShowCallout = true
+                //print("zai zhe")
+                view.image = UIImage(named:"custom_pin.png")
+                view.calloutOffset = CGPoint(x: -5, y: 5)
+                view.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure) as UIView
+                
+                let image = UIImage(named: "navigation")
+                let buttonLeft : UIButton = UIButton(type: UIButtonType.Custom)
+                buttonLeft.setImage(image, forState: UIControlState.Normal)
+                
+                buttonLeft.frame = CGRect(x: 0, y: 0, width: 60, height: 60)
+                //buttonLeft.addTarget(self, action: "funcLeftAcessoryButton:", forControlEvents: UIControlEvents.TouchUpInside)
+                
+                view.leftCalloutAccessoryView = buttonLeft
+                
+                //                let image1 = UIImage(named: "list tab")
+                //                view.leftCalloutAccessoryView = UIButton(type: .Custom).setImage(image1, forState: UIControlState.Normal) as! UIView
+                //                let btnImage = UIImage(named: "list tab")
+                //                var btn : UIButton?
+                //                let playButton  = UIButton(type: .Custom)
+                //                print("yes")
+                //                let image1 = UIImage(named: "list tab")
+                //
+                //                    playButton.setImage(let image1 = UIImage(named: "list tab"))
+                //                    view.leftCalloutAccessoryView = playButton as UIView
+                
+                
+                //
+                //                view.leftCalloutAccessoryView = btn!.setImage(btnImage , forState: UIControlState.Normal)
+                // view.leftCalloutAccessoryView = UIImageView(image: UIImage(named: "list tab"))
+            }
+            
+            // view.pinColor = annotation.pinColor()
+            
+            
+            return view
+        }
+        return nil
+    }
+    
+    //    func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlViewTapped control: UIControl) {
+    //
+    ////        if control == view.rightCalloutAccessoryView {
+    ////            self.awork = view.annotation as! ArtworkForMap
+    ////            performSegueWithIdentifier("tothemoon", sender: self)
+    ////
+    ////        }
+    //
+    //            self.awork = view.annotation as! ArtworkForMap
+    //            let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+    //            awork!.mapItem().openInMapsWithLaunchOptions(launchOptions)
+    //
+    //
+    //    }
+    
+    
+    func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        
+        if control == view.rightCalloutAccessoryView {
+            self.awork = view.annotation as! ArtworkForMap
+            performSegueWithIdentifier("tothemoon", sender: self)
+            
+        }
+        
+        if control == view.leftCalloutAccessoryView {
+            self.awork = view.annotation as! ArtworkForMap
+            let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+            awork!.mapItem().openInMapsWithLaunchOptions(launchOptions)
+        }
+        
+        
+    }
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "tothemoon"
+        {
+            
+            let controller: ViewDetailsController = segue.destinationViewController
+                as! ViewDetailsController
+            for work in reallartworks{
+                if awork?.title == work.Name{
+                    reallart = work
+                }
+                
+            }
+            
+            controller.currentArtwork = self.reallart
+            controller.hidesBottomBarWhenPushed = true
+        }
+    }
+    
+    //    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    //        if segue.identifier == "toTheMoon" {
+    //            let controller = segue.destinationViewController as! DetailViewController
+    //
+    //        }
+    //    }
+    //
+    
+    
+    //  func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+    //    let location = view.annotation as! Artwork
+    //    let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+    //    location.mapItem().openInMapsWithLaunchOptions(launchOptions)
+    //  }
+    
+}
---
+++ melbourne1/ViewDetailsController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewDetailsController.swift
+//  melbourne1
+//
+//  Created by zihaowang on 17/08/2016.
+//  Copyright © 2016 zihaowang. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+
+class ViewDetailsController: UIViewController {
+
+  
+    var currentArtwork : Artworks?
+    
+    @IBOutlet weak var textL: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self,action: #selector(imageTapped))
+        imageView.userInteractionEnabled = true
+
+        self.imageView.addGestureRecognizer(tapGestureRecognizer)
+        
+        typeLabel.lineBreakMode = NSLineBreakMode.ByWordWrapping
+        typeLabel.numberOfLines = 0;
+       self.nameLabel.text = currentArtwork?.Name
+        self.dateLabel.text = currentArtwork?.Date
+        self.artistLabel.text = currentArtwork?.Artist
+        self.typeLabel.text = currentArtwork?.Structure
+        
+            self.textL.text = currentArtwork?.Descriptions
+        self.textL.sizeToFit()
+                imageView.image = nil
+        if let photo = currentArtwork!.Photo{
+            self.imageView.loadImageUsingCacheWithUrlString(photo)}
+
+      
+        
+//        if let imageUrl = currentArtwork?.Photo {
+//            NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: imageUrl)!,completionHandler: {(data,response,error) -> Void in
+//                if error != nil {
+//                print(error)
+//                    return
+//                }
+//            let image = UIImage(data: data!)
+//                dispatch_async(dispatch_get_main_queue(), {() -> Void in
+//                    self.imageView.image = image
+//                    ;
+//                    
+//            
+//                })
+//            
+//            }).resume()
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    func heightForLabel(text:String, font:UIFont, width:CGFloat) -> CGFloat
+    {
+        let label:UILabel = UILabel(frame: CGRectMake(0, 0, width, CGFloat.max))
+        label.numberOfLines = 0
+        label.lineBreakMode = NSLineBreakMode.ByWordWrapping
+        label.font = font
+        label.text = text
+        
+        label.sizeToFit()
+        return label.frame.height
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+       func imageTapped(sender: UITapGestureRecognizer) {
+        if sender.state  == .Ended{
+        let imageView = sender.view as! UIImageView
+        let newImageView = UIImageView(image: imageView.image)
+        newImageView.frame = self.view.frame
+        newImageView.backgroundColor = .blackColor()
+        newImageView.contentMode = .ScaleAspectFit
+        newImageView.userInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(ViewDetailsController.dismissFullscreenImage(_:)))
+        newImageView.addGestureRecognizer(tap)
+        self.view.addSubview(newImageView)
+        }
+    }
+    
+    func dismissFullscreenImage(sender: UITapGestureRecognizer) {
+        sender.view?.removeFromSuperview()
+    }
+
+    
+
+}
+
