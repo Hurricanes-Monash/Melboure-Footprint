a66cde75d54e26f227c99834919d038f9c6801f1
Michael0770 <hyman0770@gmail.com>
2016-09-22 16:33:42 +1000

    yes

--- melbourne1/MapViewController.swift
+++ melbourne1/MapViewController.swift
@@ -39,8 +39,8 @@ class MapViewController: UIViewController ,CLLocationManagerDelegate{
             
         let latitude = locationManager1.location!.coordinate.latitude
         let longitude = locationManager1.location!.coordinate.longitude
-        let latDelta: CLLocationDegrees = 1
-        let lonDelta: CLLocationDegrees = 1
+        let latDelta: CLLocationDegrees = 0.1
+        let lonDelta: CLLocationDegrees = 0.1
         let span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
         let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
         let region: MKCoordinateRegion = MKCoordinateRegionMake(location, span)
