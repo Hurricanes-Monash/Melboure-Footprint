a5a676d80412658ae6fc47ed81da8470585af835
Michael0770 <hyman0770@gmail.com>
2016-10-12 20:29:27 +1100

    yes

--- ColorTemp/RootViewController.swift
+++ ColorTemp/RootViewController.swift
@@ -8,83 +8,113 @@
 
 import UIKit
 
-class RootViewController: UIViewController, UIPageViewControllerDelegate {
+class RootViewController: UIViewController, UIPageViewControllerDelegate, UIPickerViewDataSource, UIPickerViewDelegate
+ {
 
+    @IBOutlet weak var pickerView: UIPickerView!
     var pageViewController: UIPageViewController?
+    let pickerData = [
+        ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],
+        ["Color","Pressure"," Temperature"]
+    ]
 
+    var pickerDataSource = ["White", "Red", "Green", "Blue"];
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        super.viewDidLoad()
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        pickerView.selectRow(2, inComponent:sizeComponent, animated: false)
+        updateLabel()
         // Do any additional setup after loading the view, typically from a nib.
-        // Configure the page view controller and add it as a child view controller.
-        self.pageViewController = UIPageViewController(transitionStyle: .pageCurl, navigationOrientation: .horizontal, options: nil)
-        self.pageViewController!.delegate = self
-
-        let startingViewController: DataViewController = self.modelController.viewControllerAtIndex(0, storyboard: self.storyboard!)!
-        let viewControllers = [startingViewController]
-        self.pageViewController!.setViewControllers(viewControllers, direction: .forward, animated: false, completion: {done in })
-
-        self.pageViewController!.dataSource = self.modelController
-
-        self.addChildViewController(self.pageViewController!)
-        self.view.addSubview(self.pageViewController!.view)
-
-        // Set the page view controller's bounds using an inset rect so that self's view is visible around the edges of the pages.
-        var pageViewRect = self.view.bounds
-        if UIDevice.current.userInterfaceIdiom == .pad {
-            pageViewRect = pageViewRect.insetBy(dx: 40.0, dy: 40.0)
+        //Run list running jsonTest.js
+/*        let requestURL: NSURL = NSURL(string: "http://118.139.61.59:8081/listUsers")!
+        let urlRequest: NSMutableURLRequest = NSMutableURLRequest(url: requestURL as URL)
+        let session = URLSession.shared
+        let task = session.dataTask(with: urlRequest as URLRequest) {
+            (data, response, error) -> Void in
+            
+            let httpResponse = response as! HTTPURLResponse
+            let statusCode = httpResponse.statusCode
+            
+            if (statusCode == 200) {
+                print("Everyone is fine, file downloaded successfully.")
+                
+                do{
+                    
+                    let json = try JSONSerialization.jsonObject(with: data!, options:.allowFragments) as? [String: AnyObject]
+                    
+                    if let stations = json?["stations"] as? [[String: AnyObject]] {
+                        
+                        for station in stations {
+                            
+                            if let name = station["stationName"] as? String {
+                                
+                                if let year = station["buildYear"] as? String {
+                                    print(name,year)
+                                }
+                                
+                            }
+                        }
+                        
+                    }
+                    
+                }catch {
+                    print("Error with Json: \(error)")
+                }
+                
+            }
         }
-        self.pageViewController!.view.frame = pageViewRect
-
-        self.pageViewController!.didMove(toParentViewController: self)
+        
+        task.resume() */
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  
+    
+    @IBOutlet weak var pizzaLabel: UILabel!
+   
+    
+    let sizeComponent = 0
+    let toppingComponent = 1
+    
+    
+    
+    //MARK: - Picker View Data Sources and Delegates
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return pickerData.count
     }
-
-    var modelController: ModelController {
-        // Return the model controller object, creating it if necessary.
-        // In more complex implementations, the model controller may be passed to the view controller.
-        if _modelController == nil {
-            _modelController = ModelController()
-        }
-        return _modelController!
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData[component].count
     }
-
-    var _modelController: ModelController? = nil
-
-    // MARK: - UIPageViewController delegate methods
-
-    func pageViewController(_ pageViewController: UIPageViewController, spineLocationFor orientation: UIInterfaceOrientation) -> UIPageViewControllerSpineLocation {
-        if (orientation == .portrait) || (orientation == .portraitUpsideDown) || (UIDevice.current.userInterfaceIdiom == .phone) {
-            // In portrait orientation or on iPhone: Set the spine position to "min" and the page view controller's view controllers array to contain just one view controller. Setting the spine position to 'UIPageViewControllerSpineLocationMid' in landscape orientation sets the doubleSided property to true, so set it to false here.
-            let currentViewController = self.pageViewController!.viewControllers![0]
-            let viewControllers = [currentViewController]
-            self.pageViewController!.setViewControllers(viewControllers, direction: .forward, animated: true, completion: {done in })
-
-            self.pageViewController!.isDoubleSided = false
-            return .min
-        }
-
-        // In landscape orientation: Set set the spine location to "mid" and the page view controller's view controllers array to contain two view controllers. If the current page is even, set it to contain the current and next view controllers; if it is odd, set the array to contain the previous and current view controllers.
-        let currentViewController = self.pageViewController!.viewControllers![0] as! DataViewController
-        var viewControllers: [UIViewController]
-
-        let indexOfCurrentViewController = self.modelController.indexOfViewController(currentViewController)
-        if (indexOfCurrentViewController == 0) || (indexOfCurrentViewController % 2 == 0) {
-            let nextViewController = self.modelController.pageViewController(self.pageViewController!, viewControllerAfter: currentViewController)
-            viewControllers = [currentViewController, nextViewController!]
-        } else {
-            let previousViewController = self.modelController.pageViewController(self.pageViewController!, viewControllerBefore: currentViewController)
-            viewControllers = [previousViewController!, currentViewController]
-        }
-        self.pageViewController!.setViewControllers(viewControllers, direction: .forward, animated: true, completion: {done in })
-
-        return .mid
+    
+    func pickerView(_
+        pickerView: UIPickerView,
+                    titleForRow row: Int,
+                    forComponent component: Int
+        ) -> String? {
+        return pickerData[component][row]
     }
-
-
+    
+    func pickerView(_
+        pickerView: UIPickerView,
+                    didSelectRow row: Int,
+                    inComponent component: Int)
+    {
+        updateLabel()
+    }
+    
+    //MARK: - Instance Methods
+    func updateLabel(){
+        let size = pickerData[sizeComponent][pickerView.selectedRow(inComponent: sizeComponent)]
+        let topping = pickerData[toppingComponent][pickerView.selectedRow(inComponent: toppingComponent)]
+        pizzaLabel.text = "You are looking for: " + size + " updates of " + topping
+    }
+ 
+       
 }
+
+
+
 
